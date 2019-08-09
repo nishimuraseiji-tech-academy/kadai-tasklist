6dee865fa58a18c3fd9008b57e14c53b15ecc435
EC2 Default User <ec2-user@ip-172-31-94-177.ec2.internal>
2019-08-09 01:33:20 +0000

    user login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,8 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    //変更↓
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
