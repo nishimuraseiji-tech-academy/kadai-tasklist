2a2b34c774484413f281d578bc679925af34284c
EC2 Default User <ec2-user@ip-172-31-94-177.ec2.internal>
2019-08-11 06:46:06 +0000

    heroku test

--- database/migrations/2019_08_09_055220_create_tasklist_table.php
+++ database/migrations/2019_08_09_055220_create_tasklist_table.php
@@ -11,6 +11,7 @@ class CreateTasklistTable extends Migration
      *
      * @return void
      */
+    // 変更 
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
